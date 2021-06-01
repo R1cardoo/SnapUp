77791bac33735f368578d5f0370e2c554c27bbc9
lipengfei.0816 <lipengfei.0816@bytedance.com>
2021-06-01 21:52:49 +0800

    adding essential activities

---
+++ SnapUp_Android/app/src/main/java/com/example/snapup_android/completeOrder/completeOrderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.snapup_android.completeOrder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.snapup_android.R
+
+class completeOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complete_order)
+    }
+}
