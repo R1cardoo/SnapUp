9eaa73729451726ed530009b80d054e5e73ed502
Ricardo_Li <1127676571@qq.com>
2021-06-10 20:17:45 +0800

    fix recyclerview

--- SnapUp_Android/app/src/main/java/com/example/snapup_android/viewInfo/viewInfoActivity.kt
+++ SnapUp_Android/app/src/main/java/com/example/snapup_android/viewInfo/viewInfoActivity.kt
@@ -1,11 +1,12 @@
 package com.example.snapup_android.viewInfo
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import com.example.snapup_android.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.example.snapup_android.R
+
 
 class viewInfoActivity : AppCompatActivity() {
 
@@ -14,7 +15,8 @@ class viewInfoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_info)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        //val scheduleList = ScheduleList.newInstance(5)
+        val scheduleList = ScheduleList.newInstance(5)
+
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
