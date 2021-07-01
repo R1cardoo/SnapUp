87605cacdfd51e65d8f1b03c2dc45c4db9b3ee39
Ricardo_Li <1127676571@qq.com>
2021-07-01 16:01:20 +0800

    feature(Android): Construct feedback activity

--- SnapUp_Android/app/src/main/java/com/example/snapup_android/feedback/feedbackActivity.kt
+++ SnapUp_Android/app/src/main/java/com/example/snapup_android/feedback/feedbackActivity.kt
@@ -1,10 +1,11 @@
 package com.example.snapup_android.feedback
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.snapup_android.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class feedbackActivity : AppCompatActivity() {
 
@@ -13,6 +14,11 @@ class feedbackActivity : AppCompatActivity() {
         setContentView(R.layout.activity_feedback)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+
+
+        findViewById<Button>(R.id.Publish).setOnClickListener {  view ->
+            val a = 0
+        }
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
