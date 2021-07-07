9c7cfe498f1cd98919530e815bf6a1529214a013
FrozenYogurtPuff <frozenyogurtpuff@gmail.com>
2021-07-07 17:59:53 +0800

    feat(frontend/railway-service): add router for train lines and passenger credit

--- frontend/railway-service/src/config/router.config.js
+++ frontend/railway-service/src/config/router.config.js
@@ -43,6 +43,28 @@ export const asyncRouterMap = [
           }
         ]
       },
+
+      {
+        path: '/train',
+        redirect: '/train/lines',
+        component: RouteView,
+        meta: { title: 'menu.train', icon: 'control', permission: [ 'train' ] },
+        children: [
+          {
+            path: '/train/lines',
+            name: 'Lines',
+            component: () => import('@/views/train/Lines'),
+            meta: { title: 'menu.train.line', keepAlive: false, permission: [ 'train' ] }
+          },
+          {
+            path: '/train/credit',
+            name: 'Credit',
+            component: () => import('@/views/train/Credit'),
+            meta: { title: 'menu.train.credit', keepAlive: false, permission: [ 'train' ] }
+          }
+        ]
+      },
+
       // forms
       {
         path: '/form',
---
+++ frontend/railway-service/src/views/train/Lines.vue
@@ -0,0 +1,14 @@
+<template>
+  <page-header-wrapper>
+  </page-header-wrapper>
+</template>
+
+<script>
+export default {
+  name: 'lines'
+}
+</script>
+
+<style scoped>
+
+</style>
