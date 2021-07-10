8966d2e5c2101d31cd0fab96ae3f54c0feeff0b2
Ricardo_Li <1127676571@qq.com>
2021-07-10 11:59:43 +0800

    feat(Android): improve register and settings module

--- SnapUp_Android/app/src/main/java/com/example/snapup_android/login/RegisterActivity.kt
+++ SnapUp_Android/app/src/main/java/com/example/snapup_android/login/RegisterActivity.kt
@@ -3,7 +3,10 @@ package com.example.snapup_android.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.snapup_android.Homepage.HomepageActivity
 import com.example.snapup_android.R
@@ -14,6 +17,17 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         val registerButton = findViewById<Button>(R.id.register_button)
+        val id = findViewById<EditText>(R.id.editText1)
+        val password = findViewById<EditText>(R.id.editText2)
+        val nickname = findViewById<EditText>(R.id.editText3)
+        val identity = findViewById<EditText>(R.id.editText4)
+
+        id.afterTextChanged {
+            if(id.length()>5 && password.length()>5) registerButton.isEnabled = true
+        }
+        password.afterTextChanged {
+            if(id.length()>5 && password.length()>5) registerButton.isEnabled = true
+        }
         registerButton.setOnClickListener {
             //网络请求，是否成功？？？ 更改isSuccess
             val isSuccess = true
@@ -30,5 +44,19 @@ class RegisterActivity : AppCompatActivity() {
 
         }
 
+    }
+    /**
+     * Extension function to simplify setting an afterTextChanged action to EditText components.
+     */
+    fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable?) {
+                afterTextChanged.invoke(editable.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        })
     }
 }
--- SnapUp_Android/app/src/main/java/com/example/snapup_android/settings/SettingsActivity.kt
+++ SnapUp_Android/app/src/main/java/com/example/snapup_android/settings/SettingsActivity.kt
@@ -2,12 +2,34 @@ package com.example.snapup_android.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType.TYPE_CLASS_TEXT
+import android.widget.Button
+import android.widget.EditText
 import com.example.snapup_android.R
+import kotlinx.android.synthetic.main.activity_register.textView1
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val edit = findViewById<Button>(R.id.Edit_Settings)
+        val upload = findViewById<Button>(R.id.Upload_Settings)
+        val id = findViewById<EditText>(R.id.editText1)
+        val password = findViewById<EditText>(R.id.editText2)
+        val nickname = findViewById<EditText>(R.id.editText3)
+        val identity = findViewById<EditText>(R.id.editText4)
+
+        edit.setOnClickListener {
+            id.inputType = TYPE_CLASS_TEXT
+            password.inputType = TYPE_CLASS_TEXT
+            nickname.inputType = TYPE_CLASS_TEXT
+            identity.inputType = TYPE_CLASS_TEXT
+        }
+
+        upload.setOnClickListener {
+            //提交修改！！
+        }
 
     }
 }
